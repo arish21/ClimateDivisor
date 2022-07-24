32d29a61950980b2e271eb43aee180467d41467c
arish21 <arishdakhani99@gmail.com>
2022-07-23 21:18:03 -0400

    Intitial commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="heading-intro">Bonjour. Hello!</h1>
+        <h4 className="heading-info">
+          Please navigate through the app using the navigation bar.
+        </h4>
+        <span className="or-line1"><div className="or">OR</div></span><p className="or-line2"></p>
+        
+      </div>
+      
+        
+      </div>
+  );
+};
+
+export default Welcome;
