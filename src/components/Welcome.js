31d5a55588cb6b36b26a448ca3c92ae13c0b9fb0
arish21 <arishdakhani99@gmail.com>
2022-07-25 02:59:30 -0400

    Added animating gradient text

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,8 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+
 
 const Welcome = () => {
   return (
