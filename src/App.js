2c95d2e3c1cde918b7795971caa0eb6784dbd18f
arish21 <arishdakhani99@gmail.com>
2022-07-26 03:26:59 -0400

    Login page layout completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
 
 import './App.css';
+import Login from './components/Login';
 import NavigationBar from './components/NavigationBar';
 import Welcome from './components/Welcome';
 import './css/Welcome.css';
+import './css/Login.css'
 
 function App() {
   return (
     <>
-    <NavigationBar />
-    <Welcome />
-    
+    {/* <NavigationBar /> */}
+    {/* <Welcome /> */}
+    <Login />
     </>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { FcGoogle } from "react-icons/fc";
+import { DiApple } from "react-icons/di";
+import { FaFacebook } from "react-icons/fa";
+
+function TextInput({ type = "text", label }) {
+  const [value, setValue] = useState("");
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return (
+    <div className="input-container">
+      <input type={type} value={value} onChange={handleChange} />
+      <label className={value && "filled"}>{label}</label>
+    </div>
+  );
+}
+
+const Login = () => {
+  return (
+    <div className="centered-div">
+      <div className="centered-login-box">
+        <h3 className="login-heading">Hello again!</h3>
+        <p className="login-info">Login with your credentials</p>
+        <form className="form-css">
+          <TextInput label="Email address" />
+          <TextInput type="password" label="Password" />
+        </form>
+        <div className="form-text">
+          <Form.Text className="text">
+            <u>Forgot Password?</u>
+          </Form.Text>
+        </div>
+        <div className="login-btn-area">
+          <Button variant="btn login-btn ">Login</Button>
+        </div>
+        <div className="cancel-btn">
+          <Button variant="btn cancel-b">
+            <u>Cancel</u>
+          </Button>
+        </div>
+        <div className="member-area">
+          Not a member?{" "}
+          <a href="/">
+            <u>Register Now</u>
+          </a>
+        </div>
+        <div className="or-area">
+          <p className="line">
+            <span>Or continue with</span>
+          </p>
+        </div>
+        <div className="icons">
+          <span>
+            <button className="icon-btns"><FcGoogle style={{ fontSize: "35px" }} /></button>
+          </span>
+          <span>
+            <button className="icon-btns"><DiApple style={{ fontSize: "35px" }} /></button>
+          </span>
+          <span>
+            <button className="icon-btns"><FaFacebook style={{ color: "#4267B2", fontSize: "35px" }} /></button>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
